af8bdf406c994e795d16f39724d11ae8b4c43590
liuzhaicutey <gracebaguio64@gmail.com>
2025-12-10 20:22:01 +0800

    dllmain.cpp

--- Watchdog-ProcessTermination/dllmain.cpp
+++ Watchdog-ProcessTermination/dllmain.cpp
@@ -1,85 +1,139 @@
 #include "pch.h"
 #include <windows.h>
 #include <tlhelp32.h>
-#include <string>
 #include <set>
+#include <string>
+#include <fstream>
 
-void ShowPermissionError() {
-    MessageBox(NULL, L"You don't have enough permission to run this process.", L"Permission Denied", MB_OK | MB_ICONERROR);
+#define LOG_PATH L"C:\\process_monitor_log.txt" // Debugger. Remove if not necessary.
+
+
+void Log(const std::wstring& msg)
+{
+    std::wofstream file(LOG_PATH, std::ios::app);
+    if (file.is_open())
+        file << msg << std::endl;
 }
 
-std::set<std::wstring> GetRunningProcessNames() {
-    std::set<std::wstring> processNames;
-    PROCESSENTRY32 processEntry;
-    processEntry.dwSize = sizeof(PROCESSENTRY32);
 
-    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (snapshot == INVALID_HANDLE_VALUE) {
-        return processNames;
+BOOL EnableDebugPrivilege()
+{
+    HANDLE hToken;
+    TOKEN_PRIVILEGES tp;
+
+    if (!OpenProcessToken(GetCurrentProcess(),
+        TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken))
+    {
+        Log(L"OpenProcessToken failed");
+        return FALSE;
     }
 
-    BOOL found = Process32First(snapshot, &processEntry);
-    while (found) {
-        processNames.insert(processEntry.szExeFile);
-        found = Process32Next(snapshot, &processEntry);
+    LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &tp.Privileges[0].Luid);
+
+    tp.PrivilegeCount = 1;
+    tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+    AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(tp), NULL, NULL);
+
+    CloseHandle(hToken);
+
+    if (GetLastError() != ERROR_SUCCESS)
+    {
+        Log(L"AdjustTokenPrivileges failed");
+        return FALSE;
     }
 
-    CloseHandle(snapshot);
-    return processNames;
+    Log(L"SeDebugPrivilege enabled");
+    return TRUE;
 }
 
-void TerminateProcessByName(const wchar_t* processName) {
-    PROCESSENTRY32 processEntry;
-    processEntry.dwSize = sizeof(PROCESSENTRY32);
 
-    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (snapshot == INVALID_HANDLE_VALUE) {
+std::set<DWORD> GetProcessIDs()
+{
+    std::set<DWORD> result;
+    PROCESSENTRY32 pe32;
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hSnap == INVALID_HANDLE_VALUE)
+    {
+        Log(L"CreateToolhelp32Snapshot failed");
+        return result;
+    }
+
+    if (Process32First(hSnap, &pe32))
+    {
+        do {
+            result.insert(pe32.th32ProcessID);
+        } while (Process32Next(hSnap, &pe32));
+    }
+
+    CloseHandle(hSnap);
+    return result;
+}
+
+
+void KillPID(DWORD pid)
+{
+    if (pid == 0)
+        return;
+
+    HANDLE hProc = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
+    if (!hProc)
+    {
+        Log(L"OpenProcess failed for PID " + std::to_wstring(pid));
         return;
     }
 
-    BOOL found = Process32First(snapshot, &processEntry);
-    while (found) {
-        if (_wcsicmp(processEntry.szExeFile, processName) == 0) {
-            HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, processEntry.th32ProcessID);
-            if (hProcess != NULL) {
-                TerminateProcess(hProcess, 0);
-                CloseHandle(hProcess);
-            }
-        }
-        found = Process32Next(snapshot, &processEntry);
-    }
+    if (!TerminateProcess(hProc, 0))
+        Log(L"TerminateProcess FAILED for PID " + std::to_wstring(pid));
+    else
+        Log(L"Process terminated: PID " + std::to_wstring(pid));
 
-    CloseHandle(snapshot);
+    CloseHandle(hProc);
 }
 
-DWORD WINAPI MonitorProcess(LPVOID lpParam) {
-    std::set<std::wstring> initialProcessNames = GetRunningProcessNames();
-    while (TRUE) {
-        std::set<std::wstring> currentProcessNames = GetRunningProcessNames();
+
+DWORD WINAPI MonitorThread(LPVOID)
+{
+    Log(L"MonitorThread started");
 
 
-        for (const auto& processName : currentProcessNames) {
-            if (initialProcessNames.find(processName) == initialProcessNames.end()) {
-                TerminateProcessByName(processName.c_str());
-                ShowPermissionError();
+    std::set<DWORD> knownPIDs = GetProcessIDs();
+
+    while (true)
+    {
+        std::set<DWORD> current = GetProcessIDs();
+
+
+        for (DWORD pid : current)
+        {
+            if (knownPIDs.count(pid) == 0)
+            {
+                Log(L"New process detected: PID " + std::to_wstring(pid));
+                KillPID(pid);
             }
         }
 
-        initialProcessNames = currentProcessNames;
+        knownPIDs = current;
 
-        Sleep(1000);
+        Sleep(500);
     }
+
     return 0;
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
-    switch (ul_reason_for_call) {
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID)
+{
+    switch (reason)
+    {
     case DLL_PROCESS_ATTACH:
-        CreateThread(NULL, 0, MonitorProcess, NULL, 0, NULL);
-        break;
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
+        DisableThreadLibraryCalls(hModule);
+
+        EnableDebugPrivilege();
+
+        CreateThread(NULL, 0, MonitorThread, NULL, 0, NULL);
+
         break;
     }
     return TRUE;
